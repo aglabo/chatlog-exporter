9b839f6e1f8b6e4c902700560f6ae6d8504a7373
Furukawa Atsushi <atsushifx@gmail.com>
2026-06-29 20:23:42 +0900

    chore(types/file-io): remove file io types file
    - skills/_scripts/types/file-io.types.ts:
      file-io ライブラリ専用の型定義ファイルを削除

--- skills/_scripts/types/file-io.types.ts
+++
@@ -1,7 +0,0 @@
-// src: skills/_scripts/types/file-io.types.ts
-// @(#): file-io ライブラリ専用の型定義
-//
-// Copyright (c) 2026- atsushifx <https://github.com/atsushifx>
-//
-// This software is released under the MIT License.
-// https://opensource.org/licenses/MIT
